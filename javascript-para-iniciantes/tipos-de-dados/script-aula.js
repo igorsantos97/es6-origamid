1fefd99dc7ca489de323117edef2d3aedcd0fcfc
Igor Santos <igornzds@gmail.com>
2020-08-15 13:47:39 -0300

    Finalizar exemplos de tipos de dados e iniciar exercicios

---
+++ javascript-para-iniciantes/tipos-de-dados/script-aula.js
@@ -0,0 +1,38 @@
+
+var idade = 23;
+var simbolo = Symbol();
+
+console.log(typeof simbolo);
+
+
+var nome = 'Igor';
+var sombrenome = 'Santos';
+var nomeCompleto = nome + ' ' + sombrenome;
+
+console.log(nomeCompleto);
+
+
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols';
+
+console.log(typeof frase);
+
+var ano = 2020;
+var mes = 08;
+
+
+console.log(ano + mes);
+
+
+var melhor = 'teste';
+var frase1 = 'Esse é o '+ melhor + ' jogo';
+
+console.log(frase1);
+console.log('Isso mesmo');
+
+// Template String
+var gols = 1000;
+var frase = `Romário fez ${gols} gols`;
+
+console.log(frase)
