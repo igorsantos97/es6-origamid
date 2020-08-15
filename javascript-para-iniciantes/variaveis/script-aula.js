987b8d547c183d34a6be0a9e25a7c1313888d364
Igor Santos <igornzds@gmail.com>
2020-08-14 22:48:11 -0300

    Finalizar exemplos de variáveis e iniciar exercicios

---
+++ javascript-para-iniciantes/variaveis/script-aula.js
@@ -0,0 +1,29 @@
+var nome = 'Igor';
+var idade = 22;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+let sobrenome = "Santos",
+    estado = "SP";
+
+console.log(sobrenome, estado);
+
+
+// Hosting
+var semValor;
+console.log(semValor); // undefined
+
+
+// Declaração de variaveis
+var time = 'Santos';
+var time = 'SP';
+
+console.log(time);
+
