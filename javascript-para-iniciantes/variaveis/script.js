e508235a48a003edefefe60fec0bc166055494e8
Igor Santos <igornzds@gmail.com>
2020-08-14 22:51:05 -0300

    Finalizar exercicios de variáveis

--- javascript-para-iniciantes/variaveis/script.js
+++ javascript-para-iniciantes/variaveis/script.js
@@ -1,10 +1,15 @@
 // Declarar uma variável com o seu nome
+var nome = 'Igor';
 
 // Declarar uma variável com a sua idade
+var idade = 22;
 
 // Declarar uma variável com a sua comida
 // favorita e não atribuir valor
+var comida;
 
 // Atribuir valor a sua comida favorita
+comida = 'Lasanha';
 
 // Declarar 5 variáveis diferentes sem valores
+var v1, v2, v3, v4, v5;
