81cce596f86b4dd13dafff839b401ec2b1ded52f
Igor Santos <igornzds@gmail.com>
2020-08-15 14:57:04 -0300

    Finalizar exemplos de numeros e operadores & Iniciar exercicios

---
+++ javascript-para-iniciantes/numeros-e-operadores/script-aula.js
@@ -0,0 +1,42 @@
+var total = 10 + 5;
+var divisao = 200 / 100;
+var modulo = 9 % 3;
+
+console.log(isNaN('modulo'));
+
+ 
+var testeNan = '100' / 2;
+// console.log(isNaN(testeNan));
+
+var soma1 = (10 * 4) / 2 + 10;
+// console.log(soma1);
+
+
+// Operadores unários
+var x = 5;
+++x; 
+// console.log(x);
+
+var idade = +'22';
+var somaIdade = 3;
+
+console.log(idade + -somaIdade);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
