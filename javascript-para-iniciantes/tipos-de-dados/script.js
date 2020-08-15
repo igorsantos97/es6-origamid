93f0697db8c293f8952cf4a84a2637a84d9d9e3d
Igor Santos <igornzds@gmail.com>
2020-08-15 14:04:59 -0300

    Finalizar exercicios de tipos de dados

--- javascript-para-iniciantes/tipos-de-dados/script.js
+++ javascript-para-iniciantes/tipos-de-dados/script.js
@@ -1,12 +1,22 @@
 // Declare uma variável contendo uma string
+var frase = 'Essa é uma frase';
 
 // Declare uma variável contendo um número dentro de uma string
+var texto = '25';
 
 // Declare uma variável com a sua idade
+var idade = 22;
 
 // Declare duas variáveis, uma com seu nome
 // e outra com seu sobrenome e some as mesmas
+var nome = 'Igor',
+    sobrenome = 'Santos';
+
+// console.log(nome + ' ' + sobrenome);     
+console.log(`${nome} ${sobrenome}`);
 
 // Coloque a seguinte frase em uma variável: It's time
+var frase1 = `It's time`;
 
 // Verifique o tipo da variável que contém o seu nome
+console.log(typeof frase1);
