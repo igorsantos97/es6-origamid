399f1f06f647bb13421bbb8aa46fe2ff4446e612
Igor Santos <igornzds@gmail.com>
2020-08-15 22:28:15 -0300

    Finalizar exercicios de boolean e condicionais

--- javascript-para-iniciantes/boolean-e-condicionais/script.js
+++ javascript-para-iniciantes/boolean-e-condicionais/script.js
@@ -1,30 +1,48 @@
 // Verifique se a sua idade é maior do que a de algum parente
 // Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 22;
+var idadeParente = 50;
+
+if(minhaIdade > idadeParente) {
+  console.log('É maior');
+} else if(minhaIdade === idadeParente) {
+    console.log('É igual');
+} else { //true
+    console.log('É menor'); 
+}
 
 // Qual valor é retornado na seguinte expressão?
-var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2); // 3
 
 // Verifique se as seguintes variáveis são Truthy ou Falsy
-var nome = 'Andre';
-var idade = 28;
-var possuiDoutorado = false;
-var empregoFuturo;
-var dinheiroNaConta = 0;
+var nome = 'Andre'; //true
+var idade = 28; //true
+var possuiDoutorado = false; //false
+var empregoFuturo; //false
+var dinheiroNaConta = 0; //false
+
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta)
 
 // Compare o total de habitantes do Brasil com China (valor em milhões)
 var brasil = 207;
 var china = 1340;
 
+if(brasil > china) {
+  console.log('Brasil tem mais habitantes');
+} else { 
+  console.log('China tem mais habitantes');
+}
+
 // O que irá aparecer no console?
 if(('Gato' === 'gato') && (5 > 2)) {
   console.log('Verdadeiro');
 } else {
-  console.log('Falso');
+  console.log('Falso'); // Esse
 }
 
 // O que irá aparecer no console?
 if(('Gato' === 'gato') || (5 > 2)) {
-  console.log('Gato' && 'Cão');
+  console.log('Gato' && 'Cão'); // Cão
 } else {
   console.log('Falso');
 }
