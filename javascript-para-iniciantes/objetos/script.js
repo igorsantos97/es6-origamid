6ca0adba7ee7d69377057cf7c8d174ea361b0001
Igor Santos <igornzds@gmail.com>
2020-08-17 15:23:31 -0300

    Ajustar código no exercicio de objetos

--- javascript-para-iniciantes/objetos/script.js
+++ javascript-para-iniciantes/objetos/script.js
@@ -1,17 +1,40 @@
 // Crie um objeto com os seus dados pessoais
-// Deve possui pelo menos duas propriedades nome e sobrenome
-
+// Deve possuir pelo menos duas propriedades nome e sobrenome
+var dadosPessoais = {
+    nome: 'Igor',
+    sobrenome: 'Santos',
+}
 
 // Crie um método no objeto anterior, que mostre o seu nome completo
+dadosPessoais.nomeCompleto = function() {
+    console.log(`${this.nome} ${this.sobrenome}`);
+}
+
+dadosPessoais.nomeCompleto();
 
 // Modifique o valor da propriedade preco para 3000
 var carro = {
     preco: 1000,
     portas: 4,
     marca: 'Audi',
-  }
-  
-  // Crie um objeto de um cachorro que represente um labrador,
-  // preto com 10 anos, que late ao ver um homem
-  
-  
+}
+
+carro.preco = 3000;
+
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorro = {
+    raca: 'Labrado',
+    cor: 'Preto',
+    idade: 10, 
+    latir(pessoa) {
+        if(pessoa === 'homem'){
+            return 'Cachorro Latiu';
+        } else {
+            return 'Não fez nada';
+        }
+    } 
+}
+
+console.log(cachorro.latir('homem'));
+
