1253969da05f56250867cd700324ef74b0389390
Igor Santos <igornzds@gmail.com>
2020-08-18 14:46:19 -0300

    Terminar exercícios de introdução de DOM

--- dom-para-iniciantes/o-que-e-o-dom/script.js
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -1,8 +1,21 @@
 // Retorne o url da página atual utilizando o objeto window
+const url = window.location.href; // Não precisa colocar o objeto 'window'
+
+console.log(url);
 
 // Seleciona o primeiro elemento da página que
 // possua a classe ativo
+const firstElementAtivo = document.querySelector('.ativo');
+
+console.log(firstElementAtivo);
 
 // Retorne a linguagem do navegador
+const languageNavigator = navigator.language;
+
+console.log(languageNavigator);
 
 // Retorne a largura da janela 
+const widthWindow = innerWidth;
+
+console.log(widthWindow);
+
