1d5962c85acb7fb11b217a56a6b50e457f7388a8
Igor Santos <igornzds@gmail.com>
2020-08-18 14:54:56 -0300

    Adicionando uma dica ao ultimo exercício feito

--- dom-para-iniciantes/o-que-e-o-dom/script.js
+++ dom-para-iniciantes/o-que-e-o-dom/script.js
@@ -1,5 +1,5 @@
 // Retorne o url da página atual utilizando o objeto window
-const url = window.location.href; // Não precisa colocar o objeto 'window'
+const url = window.location.href; // Não precisa colocar o objeto 'window', pois ele é o objeto máximo
 
 console.log(url);
 
@@ -17,5 +17,5 @@ console.log(languageNavigator);
 // Retorne a largura da janela 
 const widthWindow = innerWidth;
 
-console.log(widthWindow);
+console.log(widthWindow); // Com esse valor eu posso verificar se o usuário está com o tamanho de tela maior que: 800px, eu aplico "efeitos" com movimento com mouse
 
