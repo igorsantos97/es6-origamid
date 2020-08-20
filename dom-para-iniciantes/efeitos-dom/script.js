9e412248e1ee2232a38b3859dedc426a0a8465ec
Igor Santos <igornzds@gmail.com>
2020-08-20 12:26:41 -0300

    Finalizar accordion list

--- dom-para-iniciantes/efeitos-dom/script.js
+++ dom-para-iniciantes/efeitos-dom/script.js
@@ -1,26 +1,51 @@
-const tabmenu = document.querySelectorAll('.js-tabmenu li');
-const sectionTabcontent = document.querySelectorAll('.js-tabcontent section');
+function initTabnav() {
+    const tabmenu = document.querySelectorAll('.js-tabmenu li');
+    const sectionTabcontent = document.querySelectorAll('.js-tabcontent section');
 
-if(tabmenu.length && sectionTabcontent.length) {
-    sectionTabcontent[0].classList.add('active');
+    if (tabmenu.length && sectionTabcontent.length) {
+        sectionTabcontent[0].classList.add('active');
 
-    function handleTabmenu(item, index) {
-        sectionTabcontent.forEach((section) => {
-            section.classList.remove('active');
+        function handleTabmenu(item, index) {
+            sectionTabcontent.forEach((section) => {
+                section.classList.remove('active');
+            });
+
+            sectionTabcontent[index].classList.add('active');
+        }
+
+        tabmenu.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                handleTabmenu(item, index);
+            });
         });
-
-        sectionTabcontent[index].classList.add('active');
     }
-
-    tabmenu.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            handleTabmenu(item, index);
-        });
-    });
 }
 
 
+function initAccordionList() {
+    const active = 'active';
+    const dtAccordion = document.querySelectorAll('.js-accordion dt');
 
+    if (dtAccordion.length) {
+        dtAccordion[0].classList.add(active);
+        dtAccordion[0].nextElementSibling.classList.add(active);
+
+        function toggleItemAccordion() {
+            const elementTarget = this; // como não tem vários itens (apenas um) uso apenas 'this'
+            const dlItem = elementTarget.nextElementSibling; // não necessita de colocar em uma 'const', pode fazer direto
+
+            elementTarget.classList.toggle(active);
+            dlItem.classList.toggle(active);
+        }
+
+        dtAccordion.forEach((item) => {
+            item.addEventListener('click', toggleItemAccordion);
+        });
+    }
+}
+
+initTabnav();
+initAccordionList();
 
 
 
