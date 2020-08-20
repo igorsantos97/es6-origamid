02a82338694df8200e88d8c60229142ee29c3be0
Igor Santos <igornzds@gmail.com>
2020-08-20 12:48:15 -0300

    Finalizar scroll suave (link interno)

--- dom-para-iniciantes/efeitos-dom/script.js
+++ dom-para-iniciantes/efeitos-dom/script.js
@@ -44,8 +44,28 @@ function initAccordionList() {
     }
 }
 
+function initScrollSuave() {
+    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+    if (linksInternos.length) {
+        function activeScrolSuave(e) {
+            e.preventDefault();
+            const href = e.target.getAttribute('href');
+            const section = document.querySelector(href);
+
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+
+        linksInternos.forEach((link) => link.addEventListener('click', activeScrolSuave));
+    }
+}
+
 initTabnav();
 initAccordionList();
+initScrollSuave();
 
 
 
