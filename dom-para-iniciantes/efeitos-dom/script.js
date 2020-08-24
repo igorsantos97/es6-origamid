2fd288a2858ce0ace537f92c51fa9a3f89f1212a
Igor Santos <igornzds@gmail.com>
2020-08-24 13:12:38 -0300

    Finalizar módulo de dom para iniciantes e iniciar de objetos

--- dom-para-iniciantes/efeitos-dom/script.js
+++ dom-para-iniciantes/efeitos-dom/script.js
@@ -63,9 +63,40 @@ function initScrollSuave() {
     }
 }
 
+function animacaoScroll() {
+    const sections = document.querySelectorAll('.js-scroll');
+    const windowMetade = window.innerHeight * 0.65;
+
+
+    if (sections.length) {
+        function animaSectionScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const sectionIsVisible = sectionTop - windowMetade < 0;
+
+
+                if (sectionIsVisible) {
+                    section.classList.add('active');
+                } else {
+                    section.classList.remove('active');
+                }
+            });
+        }
+
+        window.addEventListener('scroll', animaSectionScroll);
+        
+        animaSectionScroll(); // Iniciando de primeira ao iniciar a página
+    }
+
+
+}
+
+
+
 initTabnav();
 initAccordionList();
 initScrollSuave();
+animacaoScroll();
 
 
 
