da7f91ba2ce1d07e55f02fdb3d18ff1d3e3979f6
Igor Santos <igornzds@gmail.com>
2020-09-10 15:45:33 -0300

    Finalizar aula e os exercícios de native, host e user object

---
+++ objetos/native-host-e-user/script-aula.js
@@ -0,0 +1,23 @@
+console.log(typeof Array.from2 !== "undefined"); // false
+
+if (typeof Array.from !== 'undefined')
+    console.log('Existe') // Existe
+else
+    console.log('Não Existe');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ objetos/native-host-e-user/script.js
@@ -0,0 +1,21 @@
+// Liste 5 objetos nativos
+Number
+Math
+Function
+String
+Symbol
+
+// Liste 5 objetos do browser
+document
+window
+alert
+location
+setTimeout
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+// webkitHidden
+//webkitCurrentFullScreenElement
+
+if (typeof document.webkitFullscreenEnabled !== 'undefined')
+    console.log('Existe');
