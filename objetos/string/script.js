9e729f86d15c27c694810e1d63827c53098d1835
Igor Santos <igornzds@gmail.com>
2020-09-10 22:28:43 -0300

    Finalizar exercícios de String

--- objetos/string/script.js
+++ objetos/string/script.js
@@ -24,8 +24,26 @@ const transacoes = [
     },
 ];
 
+let taxaTotal = 0;
+let recebimentoTotal = 0;
+
+transacoes.forEach(({ descricao, valor }) => {
+    // OBS: Se houvesse uma palavra 'Taxa' NO MEIO da string e você só quer filtrar os que estão no começo, use o '.slice(0, 4)'
+    if (descricao.includes('Taxa')) {
+        taxaTotal += Number(valor.replace('R$ ', ''));
+    } else {
+        recebimentoTotal += Number(valor.replace('R$ ', ''));
+    }
+});
+
+console.log(recebimentoTotal, taxaTotal);
+
 // Retorne uma array com a lista abaixo
 const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
+
+const arrayTransportes = transportes.split(';');
+// console.log(arrayTransportes);
+
 
 // Substitua todos os span's por a's
 const html = `<ul>
@@ -34,10 +52,19 @@ const html = `<ul>
                   <li><span>Contato</span></li>
                 </ul>`;
 
+const htmlArray = html.split('span');
+const newHtml = htmlArray.join('a');
+
+// console.log(newHtml);
+
 // Retorne o último caracter da frase
 const frase = 'Melhor do ano!';
+// console.log(frase[frase.length - 1]); // !
 
 // Retorne o total de taxas
-const transacoes = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+const transacoes2 = ['Taxa do Banco', '   TAXA DO PÃO', '  taxa do mercado', 'depósito Bancário', 'TARIFA especial'];
+let qtdeTaxa = 0;
 
+transacoes2.forEach(item => item.trim().toLocaleLowerCase().includes('taxa') ? qtdeTaxa++ : '');
 
+console.log(qtdeTaxa); // 3
