c0e4ba30362fde039bb51da8387b585c30567fd7
Igor Santos <igornzds@gmail.com>
2020-09-14 10:00:06 -0300

    Finalizar exercícios de Number e Math (Versão Origamid)

--- objetos/number-e-math/script.js
+++ objetos/number-e-math/script.js
@@ -10,27 +10,57 @@ let numAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
 const numeros = '4, 5, 20, 8, 9';
 const arrayNumero = numeros.split(', ');
 
-// console.log(Math.max.apply(Math, arrayNumero));
-// 1 argumento do .apply -> Função
+// console.log(Math.max.apply(Math, arrayNumero)); // 20 | 1 argumento do .apply -> Função
+console.log(Math.max(...arrayNumero)); // 20 (ORIGAMID)
+
 
 
 // Crie uma função para limpar os preços
 // e retornar os números com centavos arredondados
 // depois retorne a soma total
-function somaTotal(arrayPrecos) {
-    let total = 0;
-
-    arrayPrecos.forEach((preco) => {
-        total += +preco.toUpperCase().replace('R$ ', '').replace(',', '.').trim();
-    });
+const listaPrecos = ['R$ 59,99', ' R$ 100,222', 'R$ 230  ', 'r$  200'];
 
 
-    total = Math.round(total);
+// function somaTotal(arrayPrecos) {
+//     let total = 0;
 
-    return total;
+//     arrayPrecos.forEach((preco) => {
+//         total += +preco.toUpperCase().replace('R$ ', '').replace(',', '.').trim();
+//     });
+
+
+//     total = Math.round(total);
+
+//     return total;
+// }
+
+// console.log(somaTotal(listaPrecos));
+
+
+// #### ORIGAMID
+function limparPreco(preco) {
+    preco = +preco.toUpperCase().replace('R$', '').trim().replace(',', '.');
+    preco = +preco.toFixed(2);
+
+    return preco;
 }
 
-const listaPrecos = ['R$ 59,99', ' R$ 100,222',
-    'R$ 230  ', 'r$  200'];
 
-console.log(somaTotal(listaPrecos));
+let soma = 0;
+listaPrecos.forEach((preco) => {
+    soma += limparPreco(preco);
+});
+
+console.log(soma); // 590,21
+console.log(soma.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })); // R$ 590,21
+
+
+
+
+
+
+
+
+
+
+
